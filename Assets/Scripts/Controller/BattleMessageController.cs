8bc7bcb599837d10ea4eadb1088c6edb772ca7eb
ma2000w <127324246+ma2000w@users.noreply.github.com>
2024-04-26 05:36:10 -0600

    Fixed some comment errors

--- Assets/Scripts/Controller/BattleMessageController.cs
+++ Assets/Scripts/Controller/BattleMessageController.cs
@@ -5,56 +5,47 @@ using System.Collections;
 
 public class BattleMessageController : MonoBehaviour 
 {
-	// Reference to the UI text component for displaying the battle message.
 	[SerializeField] Text label;
-	// Reference to the canvas GameObject containing the text component.
 	[SerializeField] GameObject canvas;
-	// Reference to the canvas group component for controlling transparency.
 	[SerializeField] CanvasGroup group;
-	// EasingControl component for handling alpha animation.
 	EasingControl ec;
 
-	// Called when the script instance is being loaded.
 	void Awake ()
 	{
-		// Add an EasingControl component for managing the animation.
 		ec = gameObject.AddComponent<EasingControl>();
-		ec.duration = 0.5f; // Set the duration of the animation.
-		ec.equation = EasingEquations.EaseInOutQuad; // Set the easing equation for smooth animation.
-		ec.endBehaviour = EasingControl.EndBehaviour.Constant; // Set the behavior when the animation ends.
-		ec.updateEvent += OnUpdateEvent; // Subscribe to the update event of the EasingControl.
+		ec.duration = 0.5f;
+		ec.equation = EasingEquations.EaseInOutQuad;
+		ec.endBehaviour = EasingControl.EndBehaviour.Constant;
+		ec.updateEvent += OnUpdateEvent;
 	}
 
-	// Displays the battle message.
 	public void Display (string message)
 	{
-		group.alpha = 0; // Set the initial alpha value to 0 to make the canvas invisible.
-		canvas.SetActive(true); // Activate the canvas GameObject.
-		label.text = message; // Set the text of the label to the provided message.
-		StartCoroutine(Sequence()); // Start the message display sequence.
+		group.alpha = 0;
+		canvas.SetActive(true);
+		label.text = message;
+		StartCoroutine(Sequence());
 	}
 
-	// Event handler for updating the alpha value of the canvas group.
 	void OnUpdateEvent (object sender, EventArgs e)
 	{
-		group.alpha = ec.currentValue; // Update the alpha value based on the current value of the EasingControl.
+		group.alpha = ec.currentValue;
 	}
 
-	// Controls the sequence of displaying and hiding the message.
 	IEnumerator Sequence ()
 	{
-		ec.Play(); // Start the animation.
+		ec.Play();
 
-		// Wait until the animation finishes.
 		while (ec.IsPlaying)
 			yield return null;
 
-		yield return new WaitForSeconds(1); // Wait for 1 second.
+		yield return new WaitForSeconds(1);
 
-		ec.Reverse(); // Reverse the animation to hide the message.
+		ec.Reverse();
 
-		// Wait until the animation finishes.
 		while (ec.IsPlaying)
 			yield return null;
 
-		canvas.SetActive(false); // Deactivate the canvas 
+		canvas.SetActive(false);
+	}
+}
--- Assets/Scripts/Controller/TurnOrderController.cs
+++ Assets/Scripts/Controller/TurnOrderController.cs
@@ -4,76 +4,79 @@ using System.Collections.Generic;
 
 public class TurnOrderController : MonoBehaviour 
 {
-	#region Constants
-	const int turnActivation = 1000;
-	const int turnCost = 500;
-	const int moveCost = 300;
-	const int actionCost = 200;
-	#endregion
+    #region Constants
+    const int turnActivation = 1000; // Threshold for activating a unit's turn
+    const int turnCost = 500; // Cost of a unit's turn
+    const int moveCost = 300; // Cost of moving during a turn
+    const int actionCost = 200; // Cost of taking an action during a turn
+    #endregion
 
-	#region Notifications
-	public const string RoundBeganNotification = "TurnOrderController.roundBegan";
-	public const string TurnCheckNotification = "TurnOrderController.turnCheck";
-	public const string TurnBeganNotification = "TurnOrderController.TurnBeganNotification";
-	public const string TurnCompletedNotification = "TurnOrderController.turnCompleted";
-	public const string RoundEndedNotification = "TurnOrderController.roundEnded";
-	#endregion
+    #region Notifications
+    public const string RoundBeganNotification = "TurnOrderController.roundBegan"; // Notification for the beginning of a round
+    public const string TurnCheckNotification = "TurnOrderController.turnCheck"; // Notification for checking if a unit can take a turn
+    public const string TurnBeganNotification = "TurnOrderController.TurnBeganNotification"; // Notification for the beginning of a turn
+    public const string TurnCompletedNotification = "TurnOrderController.turnCompleted"; // Notification for the completion of a turn
+    public const string RoundEndedNotification = "TurnOrderController.roundEnded"; // Notification for the end of a round
+    #endregion
 
-	#region Public
-	public IEnumerator Round ()
-	{
-		BattleController bc = GetComponent<BattleController>();;
-		while (true)
-		{
-			this.PostNotification(RoundBeganNotification);
+    #region Public Methods
+    // Coroutine for managing the rounds in the battle
+    public IEnumerator Round ()
+    {
+        BattleController bc = GetComponent<BattleController>(); // Get the BattleController component
+        while (true) // Loop indefinitely for continuous rounds
+        {
+            this.PostNotification(RoundBeganNotification); // Post notification for the beginning of a round
 
-			List<Unit> units = new List<Unit>( bc.units );
-			for (int i = 0; i < units.Count; ++i)
-			{
-				Stats s = units[i].GetComponent<Stats>();
-				s[StatTypes.CTR] += s[StatTypes.SPD];
-			}
+            List<Unit> units = new List<Unit>( bc.units ); // Create a list of units in the battle
+            for (int i = 0; i < units.Count; ++i) // Loop through each unit
+            {
+                Stats s = units[i].GetComponent<Stats>(); // Get the Stats component of the unit
+                s[StatTypes.CTR] += s[StatTypes.SPD]; // Increase the counter of the unit based on its speed
+            }
 
-			units.Sort( (a,b) => GetCounter(a).CompareTo(GetCounter(b)) );
+            units.Sort( (a,b) => GetCounter(a).CompareTo(GetCounter(b)) ); // Sort the units based on their counters
 
-			for (int i = units.Count - 1; i >= 0; --i)
-			{
-				if (CanTakeTurn(units[i]))
-				{
-					bc.turn.Change(units[i]);
-					units[i].PostNotification(TurnBeganNotification);
+            for (int i = units.Count - 1; i >= 0; --i) // Loop through each unit in reverse order
+            {
+                if (CanTakeTurn(units[i])) // Check if the unit can take a turn
+                {
+                    bc.turn.Change(units[i]); // Change the current turn to the unit
+                    units[i].PostNotification(TurnBeganNotification); // Post notification for the beginning of the unit's turn
 
-					yield return units[i];
+                    yield return units[i]; // Yield to allow the unit to take its turn
 
-					int cost = turnCost;
-					if (bc.turn.hasUnitMoved)
-						cost += moveCost;
-					if (bc.turn.hasUnitActed)
-						cost += actionCost;
+                    int cost = turnCost; // Initialize the cost of the turn
+                    if (bc.turn.hasUnitMoved) // Check if the unit has moved during its turn
+                        cost += moveCost; // Add the move cost to the total cost
+                    if (bc.turn.hasUnitActed) // Check if the unit has taken an action during its turn
+                        cost += actionCost; // Add the action cost to the total cost
 
-					Stats s = units[i].GetComponent<Stats>();
-					s.SetValue(StatTypes.CTR, s[StatTypes.CTR] - cost, false);
+                    Stats s = units[i].GetComponent<Stats>(); // Get the Stats component of the unit
+                    s.SetValue(StatTypes.CTR, s[StatTypes.CTR] - cost, false); // Deduct the cost from the unit's counter
 
-					units[i].PostNotification(TurnCompletedNotification);
-				}
-			}
-			
-			this.PostNotification(RoundEndedNotification);
-		}
-	}
-	#endregion
+                    units[i].PostNotification(TurnCompletedNotification); // Post notification for the completion of the unit's turn
+                }
+            }
+            
+            this.PostNotification(RoundEndedNotification); // Post notification for the end of a round
+        }
+    }
+    #endregion
 
-	#region Private
-	bool CanTakeTurn (Unit target)
-	{
-		BaseException exc = new BaseException( GetCounter(target) >= turnActivation );
-		target.PostNotification( TurnCheckNotification, exc );
-		return exc.toggle;
-	}
+    #region Private Methods
+    // Check if a unit can take a turn
+    bool CanTakeTurn (Unit target)
+    {
+        BaseException exc = new BaseException( GetCounter(target) >= turnActivation ); // Create a base exception with the condition if the unit can take a turn
+        target.PostNotification( TurnCheckNotification, exc ); // Post notification to check if the unit can take a turn
+        return exc.toggle; // Return the result of the check
+    }
 
-	int GetCounter (Unit target)
-	{
-		return target.GetComponent<Stats>()[StatTypes.CTR];
-	}
-	#endregion
+    // Get the counter value of a unit
+    int GetCounter (Unit target)
+    {
+        return target.GetComponent<Stats>()[StatTypes.CTR]; // Return the value of the counter from the unit's Stats component
+    }
+    #endregion
 }
